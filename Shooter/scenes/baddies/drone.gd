0a0b7e497b4cafa6ee7354582b562b901f015b90
Andrew Milburn <sidbell.069@gmail.com>
2023-09-25 17:38:59 +0100

    Collision Shapes Added

---
+++ Shooter/scenes/baddies/drone.gd
@@ -0,0 +1,7 @@
+extends CharacterBody2D
+
+var speed:int = 200
+
+func _process(_delta):
+	velocity = speed * Vector2.RIGHT
+	move_and_slide()
--- Shooter/scenes/levels/level.gd
+++ Shooter/scenes/levels/level.gd
@@ -1,18 +1,2 @@
 extends Node2D
 
-var test_array: Array[String] = ["Hello", "World", "How", "Are you?"]
-
-func _ready():
-	$Logo.rotation_degrees = 90
-
-	
-func _process(delta):
-	$Logo.rotation_degrees += 50 * delta
-	
-	if $Logo.rotation_degrees > 180:
-		$Logo.rotation_degrees = 0
-		
-	if $Logo.pos.x > 700:
-		$Logo.pos.x = 0
-		
-	
