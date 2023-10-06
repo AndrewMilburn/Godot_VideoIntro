29691837e8ed3fb3f39fac4dbf760b8c4185ad6a
Andrew Milburn <sidbell.069@gmail.com>
2023-10-06 08:11:02 +0100

    Halfway through Level Design - FOR REVERSION

--- Shooter/scenes/levels/house.gd
+++ Shooter/scenes/levels/house.gd
@@ -3,7 +3,8 @@ extends Area2D
 signal player_entered_house
 signal player_exited_house
 
-func _on_body_entered(_body):
+func _on_body_entered(body):
+	print(body.name)
 	player_entered_house.emit()
 
 func _on_body_exited(_body):
